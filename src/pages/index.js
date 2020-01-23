17128676ad654db14828c88c673516adf0880e25
Melissa Skevington <mskeving@gmail.com>
2020-01-22 21:31:22 -0800

    Remove accommodations and add description

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,10 +64,8 @@ const Schedule = () => {
   ];
   return (
     <div className="card row schedule">
-      <div className="subcard"></div>
       <div className="subcard">
-        <div className="title">Begins at Noon</div>
-
+        <div className="title">Saturday, Sept. 19th</div>
         <div className="scheduleTable">
           {schedule.map((row, i) => (
             <div className="scheduleRow" key={i}>
@@ -78,43 +76,11 @@ const Schedule = () => {
           ))}
         </div>
       </div>
-    </div>
-  );
-};
-
-const Accomodations = () => {
-  return (
-    <div className="card accommodations">
-      <div className="header">
-        <div className="title">Accomodations</div>
-        <div>Blocks reserved at two locations.</div>
-        <div>Call to book and be sure to mention our weddings.</div>
-      </div>
-      <div className="row">
-        <div className="hotel">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3295.76961789303!2d-73.72738656546608!3d43.06094684906703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x67a8679d5bb3f0c3!2sLongfellows%20Restaurant!5e0!3m2!1sen!2sus!4v1579231348270!5m2!1sen!2sus"
-            title="Longfellows"
-          ></iframe>
-          <div className="description">
-            <div>500 Union Ave</div>
-            <div>Saratoga Springs, NY</div>
-            <div>12866</div>
-            <div>0.5 miles</div>
-          </div>
-        </div>
-        <div className="hotel">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.6822429170456!2d-73.79218058365814!3d43.069150179145645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de39ca166c6c61%3A0x4d375a7f14db6c03!2sHilton%20Garden%20Inn%20Saratoga%20Springs!5e0!3m2!1sen!2sus!4v1579231884462!5m2!1sen!2sus"
-            title="Hilton Garden Inn"
-          ></iframe>
-          <div className="description">
-            <div>125 S Broadway</div>
-            <div>Saratoga Springs, NY</div>
-            <div>12866</div>
-            <div>4 miles</div>
-          </div>
-        </div>
+      <div className="subcard">
+        <div className="title">What to expect</div>
+        <div>The venue is right on Saratoga Lake, and is Missy's childhood home.</div>
+        <div>We hope you can join us for the full day Saturday and recommend traveling on Friday (or earlier!) if you can.</div>
+        <div style={{ marginTop: 25}}>fill this in more...</div>
       </div>
     </div>
   );
@@ -125,7 +91,6 @@ const App = () => (
     <Main />
     <Location />
     <Schedule />
-    <Accomodations />
   </div>
 );
 
