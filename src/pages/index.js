64a74ff6b7808b70bf056f9cffd39d42587d5d24
Melissa Skevington <mskeving@gmail.com>
2020-02-01 12:52:49 -0800

    Add hotel block accommodations back in

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,6 +119,7 @@ const Schedule = () => {
       </div>
       <div className="subcard">
         <WhatToExpect />
+        <GettingThere />
       </div>
     </div>
   );
@@ -169,33 +170,50 @@ const WhatToExpect = () => (
   </>
 );
 
-const Accommodations = () => (
-  <div>
-    <div className="title">Accommodations</div>
-    <div style={{ marginBottom: 20 }}>
-      We are still figuring out the details around accommodations, but we will
-      update you as we get them!
+const Accommodations = () => {
+  return (
+    <div className="card accommodations">
+      <div className="header">
+        <div className="title">Accommodations</div>
+        <div>Blocks reserved at two locations.</div>
+        <div>Call to book and be sure to mention our weddings.</div>
+      </div>
+      <div className="row">
+        <div className="hotel">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3295.76961789303!2d-73.72738656546608!3d43.06094684906703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x67a8679d5bb3f0c3!2sLongfellows%20Restaurant!5e0!3m2!1sen!2sus!4v1579231348270!5m2!1sen!2sus"
+            title="Longfellows"
+          ></iframe>
+          <div className="description">
+            <div>500 Union Ave</div>
+            <div>Saratoga Springs, NY</div>
+            <div>12866</div>
+            <div>0.5 miles</div>
+          </div>
+        </div>
+        <div className="hotel">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.6822429170456!2d-73.79218058365814!3d43.069150179145645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de39ca166c6c61%3A0x4d375a7f14db6c03!2sHilton%20Garden%20Inn%20Saratoga%20Springs!5e0!3m2!1sen!2sus!4v1579231884462!5m2!1sen!2sus"
+            title="Hilton Garden Inn"
+          ></iframe>
+          <div className="description">
+            <div>125 S Broadway</div>
+            <div>Saratoga Springs, NY</div>
+            <div>12866</div>
+            <div>4 miles</div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      If you have special constraints or considerations, please reach out and
-      let us know!
-    </div>
-  </div>
-);
+  )
+}
 
 const App = () => (
   <div className="container">
     <Main />
     <Location />
     <Schedule />
-    <div className="card row">
-      <div className="subcard">
-        <GettingThere />
-      </div>
-      <div className="subcard">
-        <Accommodations />
-      </div>
-    </div>
+    <Accommodations />
   </div>
 );
 
