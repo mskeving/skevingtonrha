e34c50695b5591672ec4e1c34cbe723f98692998
Melissa Skevington <mskeving@gmail.com>
2020-02-01 13:57:52 -0800

    Update copy and slight styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const Location = () => (
       className="subcard column text"
       style={{ justifyContent: "space-between", alignItems: "flex-start" }}
     >
-      <div className="address">
+      <div className="address addressLineHeight">
         <div className="title">Saratoga Springs</div>
         <div>700 Crescent Ave</div>
         <div>Saratoga Springs, NY</div>
@@ -50,7 +50,7 @@ const Location = () => (
 );
 
 const ScheduleItem = ({ time, name }) => {
-  const [hourMin, ampm] = time.split(" ");
+  const [hourMin, ampm, snarkyComment] = time.split(" ");
   let hour, min;
   if (hourMin.indexOf(":") > 0) {
     [hour, min] = hourMin.split(":");
@@ -64,6 +64,7 @@ const ScheduleItem = ({ time, name }) => {
         <span className="hour">{hour}</span>
         {min ? <span className="min">{min}</span> : null}
         <span className="ampm">{ampm}</span>
+        <span> {snarkyComment}</span>
       </div>
       <div className="spacer" />
       <div className="eventName">{name}</div>
@@ -82,7 +83,7 @@ const Schedule = () => {
     { name: "Cocktails", time: "6 pm" },
     { name: "Dinner and night party", time: "7 pm" }
   ];
-  const sundaySchedule = [{ name: "Brunch", time: "10 am" }];
+  const sundaySchedule = [{ name: "Brunch", time: "10 am (lol)" }];
   return (
     <div className="card row schedule">
       <div className="subcard">
@@ -128,19 +129,22 @@ const Schedule = () => {
 const GettingThere = () => (
   <div className="travel">
     <div className="title">Getting there</div>
-    <div className="subtitle">By air</div>
-    <div style={{ marginTop: 5, marginBottom: 20 }}>
-      Saratoga Springs is a 35 minute drive north of the Albany Airport (ALB).
-      You can fly into Albany but there are no direct flights from the Bay Area.
-      Another option is to fly into NYC and take the train (see below).
-    </div>
-    <div className="subtitle">By train</div>
-    <div style={{ marginTop: 5, marginBottom: 20 }}>
-      The Empire Service Amtrak goes from Penn Station in New York to Albany
-      every 1-2 hours, with a couple trains a day making local stops directly to
-      Saratoga Springs. The NYC to Albany trip takes around 2.5 hours, and is
-      thoroughly pleasant. Sit on the left side of the train for scenic views of
-      the Hudson Valley.
+    <div className="bodyLineHeight">
+      <div style={{ marginTop: 5, marginBottom: 20 }}>
+        Saratoga Springs is a 35 minute drive north of the Albany Airport (ALB).
+        If you have a tight schedule, we recommend flying in here if you can.
+      </div>
+      <div style={{ marginTop: 5, marginBottom: 20 }}>
+        If you have the time to take advantage of NYC, it's an easy 2.5 hour train
+        ride away. The Empire Service Amtrak goes from Penn Station to Albany
+        every 1-2 hours, with a couple trains a day making local stops directly to
+        Saratoga Springs. The train follows the Hudson River and is a beautiful
+        ride in September.
+      </div>
+      <div style={{ marginTop: 5, marginBottom: 20 }}>
+        There are a lot of fun places not too far away, such as The Adirondacks,
+        Boston, or Montreal if you have a car.
+      </div>
     </div>
   </div>
 );
@@ -148,24 +152,26 @@ const GettingThere = () => (
 const WhatToExpect = () => (
   <>
     <div className="title">What to expect</div>
-    <div style={{ marginBottom: 30 }}>
-      The venue is right on Saratoga Lake, and is at Missy's childhood home. We
-      hope you can join us for the full day Saturday and recommend traveling on
-      Friday (or earlier!) if you can.
-    </div>
-    <div style={{ marginBottom: 30 }}>
-      All of the events (both day and night) will take place outside in a
-      canopied tent, so please be prepared and have comfortable shoes and layers
-      of clothing.
-    </div>
-    <div style={{ marginBottom: 30 }}>
-      Following the more formal sit-down lunch, you'll have some unstructured
-      time to spend on the lake, play games, catch up with friends, or rest for
-      the evening activities.
-    </div>
-    <div style={{ marginBottom: 30 }}>
-      We'll reconvene for evening cocktails, a light/casual dinner, and dancing
-      into the night.
+    <div className="bodyLineHeight">
+      <div style={{ marginBottom: 30 }}>
+        The venue is right on Saratoga Lake, and is at Missy's childhood home. We
+        hope you can join us for the full day Saturday and recommend traveling on
+        Friday (or earlier!) if you can.
+      </div>
+      <div style={{ marginBottom: 30 }}>
+        All of the events (both day and night) will take place outside in a
+        canopied tent or on the lawn, so please be prepared and have comfortable
+        shoes and layers of clothing.
+      </div>
+      <div style={{ marginBottom: 30 }}>
+        The ceremony and lunch will be more formal. Following that we're hoping people
+        take advantage of the unstructured time to spend on the lake, play games,
+        catch up with friends, or rest for the evening activities.
+      </div>
+      <div style={{ marginBottom: 30 }}>
+        We'll reconvene for evening cocktails, a more casual dinner, and dancing
+        into the night. Missy will likely be wearing something shiny.
+      </div>
     </div>
   </>
 );
@@ -175,8 +181,8 @@ const Accommodations = () => {
     <div className="card accommodations">
       <div className="header">
         <div className="title">Accommodations</div>
-        <div>Blocks reserved at two locations.</div>
-        <div>Call to book and be sure to mention our weddings.</div>
+        <div className="bodyLineHeight">There are room blocks reserved at two locations. Call to book and be sure to mention the Skevington/Rha wedding.</div>
+        <div className="shuttle bodyLineHeight">A shuttle will be available to bring you back and forth from the venue.</div>
       </div>
       <div className="row">
         <div className="hotel">
@@ -184,7 +190,7 @@ const Accommodations = () => {
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3295.76961789303!2d-73.72738656546608!3d43.06094684906703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x67a8679d5bb3f0c3!2sLongfellows%20Restaurant!5e0!3m2!1sen!2sus!4v1579231348270!5m2!1sen!2sus"
             title="Longfellows"
           ></iframe>
-          <div className="description">
+          <div className="description addressLineHeight">
             <div>500 Union Ave</div>
             <div>Saratoga Springs, NY</div>
             <div>12866</div>
@@ -196,7 +202,7 @@ const Accommodations = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.6822429170456!2d-73.79218058365814!3d43.069150179145645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de39ca166c6c61%3A0x4d375a7f14db6c03!2sHilton%20Garden%20Inn%20Saratoga%20Springs!5e0!3m2!1sen!2sus!4v1579231884462!5m2!1sen!2sus"
             title="Hilton Garden Inn"
           ></iframe>
-          <div className="description">
+          <div className="description addressLineHeight">
             <div>125 S Broadway</div>
             <div>Saratoga Springs, NY</div>
             <div>12866</div>
